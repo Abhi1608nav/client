a96a150f73f335b67acbe01942dd5b7e197381ab
Abhi1608nav <abhinavshakya1608@gmail.com>
2023-04-04 16:35:19 +0530

    commit

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+
+import '../styles/App.css';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+/** react routes */
+
+const router=createBrowserRouter([
+  {
+    path :'/',
+    element:<div>Root Element</div>
+  },
+  {
+    path :'/quiz',
+    element:<div>Quiz Component</div>
+  },
+  {
+    path :'/',
+    element:<div>result Component</div>
+  },
+])
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+    </div>
+  );
+}
+
+export default App;
